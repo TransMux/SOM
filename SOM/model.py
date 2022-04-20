13e3ff629ecca272c417e6dd1f518b87ef0b878e
InEase <InEase28@gmail.com>
2022-04-21 00:42:16 +0800

    feat : finish train

--- SOM/model.py
+++ SOM/model.py
@@ -1,7 +1,9 @@
 from typing import Tuple
 
 import numpy as np
+from tqdm import tqdm
 
+from SOM.Distance import distance_functions
 from SOM.Neighbor import neighborhood_functions
 
 
@@ -13,7 +15,8 @@ class SOM:
             learning_rate: int,
             max_iterations: int,
             shuffle: bool = False,
-            neighbor_function: str = "bubble"
+            neighbor_function: str = "bubble",
+            distance_function: str = "euclidean"
     ):
         self.shuffle = shuffle
         self.size = size
@@ -22,15 +25,29 @@ class SOM:
 
         # 处理参数
         self.mutable_update = lambda origin, iteration: origin / (1 + iteration / (max_iterations / 2))
-        # 在 [-1, 1] 内生成随机初始权重
-        self.weights = np.random.randn(*size) * 2 - 1
+        # 在 [-1, 1] 内生成随机初始权重 x * y * features
+        self.weights = np.random.randn(*size, feature) * 2 - 1
         # 初始化激活图
         self.activation_map = np.zeros(size)
         # 初始化网格
         self.x_steps, self.y_steps = np.arange(size[0]), np.arange(size[1])
         self.xx, self.yy = np.meshgrid(self.x_steps, self.y_steps)
         # 初始化距离函数
+        self.distance = distance_functions[distance_function]
         self.neighborhood = neighborhood_functions[neighbor_function]
 
-    def fit(self):
-        pass
+    def fit(self, data: np.ndarray, verbose: bool = True):
+        # 训练过程
+        batch, feature = data.shape
+        assert feature == self.feature, "训练时传入的data维度与设置的不匹配"
+        # 开始训练
+        for i, x in enumerate(tqdm(data)):
+            # 计算全局距离
+            self.activation_map = self.distance(x, self.weights)
+            # 获取坐标 这里用了一个比较新颖的方法 unravel_index
+            winner = np.unravel_index(self.activation_map.argmin(), self.size)
+            # 得出更新步长
+            eta = self.mutable_update(self.learning_rate, i)
+            g = self.neighborhood(winner, 2) * eta
+            # 应用更新
+            self.weights += np.einsum('ij, ijk->ijk', g, x - self.weights)
