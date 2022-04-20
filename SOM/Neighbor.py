cdeaa256afdd34b8c83a52ff531b14be5ffa87f3
InEase <InEase28@gmail.com>
2022-04-21 00:15:48 +0800

    feat(Neighbor): static check

--- SOM/Neighbor.py
+++ SOM/Neighbor.py
@@ -1,8 +1,10 @@
 # Neighborhood Functions
+from typing import Sequence
+
 import numpy as np
 
 
-def bubble(c, sigma, x_steps, y_steps):
+def bubble(c: Sequence[int, int], sigma: int, x_steps: np.ndarray, y_steps: np.ndarray):
     # 更新给定点的上下左右一定范围内的全部点
     ax = np.logical_and(x_steps > c[0] - sigma, x_steps < c[0] + sigma)
     ay = np.logical_and(y_steps > c[1] - sigma, y_steps < c[1] + sigma)
